16372224af0c3274e5b17e4fe8dc4d9ad408b7b5
yunusfebriansyah <febriansyahyunus369@gmail.com>
2024-10-11 14:59:44 +0700

    08-blade-templates

---
+++ 08-blade-templates/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/display', function() {
+    return view('display', [
+        'heading' => '<h1>Belajar Blade Templates</h1>'
+    ]);
+});
+
+Route::get('/admin', function() {
+    return view('admin.index');
+});
