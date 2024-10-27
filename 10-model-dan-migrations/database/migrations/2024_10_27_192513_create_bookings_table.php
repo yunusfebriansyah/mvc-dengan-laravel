88fa097c1fc2ea064a0900edddbdd72c9ef45d2e
yunusfebriansyah <febriansyahyunus369@gmail.com>
2024-10-28 02:36:29 +0700

    10-model-dan-migrations

---
+++ 10-model-dan-migrations/database/migrations/2024_10_27_192513_create_bookings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('ticket_id');
+            $table->enum('status', ['pending', 'paid', 'cancelled']);
+            $table->timestamp('payment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
